3cf959cc5185cc82e4dfadefc4c198200f254fc9
Jefferson Huisa <j3ffh95@gmail.com>
2021-11-07 20:08:06 -0500

    called the setTime function and also the setInterval to call it every second

--- script.js
+++ script.js
@@ -83,3 +83,7 @@ function setTime() {
 const scale = (num, in_min, in_max, out_min, out_max) => {
   return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
 };
+
+setTime();
+
+setInterval(setTime, 1000);
